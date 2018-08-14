1522a7754ea61729c6ada02cf13b7b30eb4cfa68
Stan Grishin <stangri@melmac.net>
2018-08-13 21:24:16 -0700

    wlanblinker: shellchecked
    Signed-off-by: Stan Grishin <stangri@melmac.net>

--- wlanblinker/files/blink.sh
+++ wlanblinker/files/blink.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-if [ "$1" == "-d" ]; then daemon=1; shift; else daemon=0; fi
+if [ "$1" = "-d" ]; then daemon=1; shift; else daemon=0; fi
 LED="/sys/class/leds/${1}/brightness" display="$2" radio="$3" onTime="${4:-1}" offTime="${5:-1}" sleepTime="${6:-3}"
-[[ -e "$LED" && -n "$display" && -n "$radio" ]] || exit 1
+if [ ! -e "$LED" ] || [ -z "$display" ] || [ -z "$radio" ]; then exit 1; fi
 
 while :; do
 	i=1 blinks=0
 	case $display in
 		channel)
-			channel="$(uci -q get wireless.$radio.channel)"
-			if [ "$channel" == "auto" ]; then
+			channel="$(uci -q get wireless."$radio".channel)"
+			if [ "$channel" = "auto" ]; then
 				onTime='0.1'; offTime='0.1'; sleepTime='0.1'; blinks='2';
 			else
 				blinks="$channel"
 			fi
 			;;
 		link_quality)
-			lq="$(iwinfo $radio info | grep 'Link Quality' | awk '{print $6}')"
+			lq="$(iwinfo "$radio" info | grep 'Link Quality' | awk '{print $6}')"
 			lqbase="${lq##*/}"; lq="${lq%/*}"; blinks=$((lq * 10 / lqbase));
 			;;
 	esac
