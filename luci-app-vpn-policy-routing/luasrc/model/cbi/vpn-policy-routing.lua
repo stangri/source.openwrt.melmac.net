d93f3e741ed2c10c8a0340eaae291d41c74f7731
Stan Grishin <stangri@melmac.net>
2019-08-10 22:22:08 -0700

    vpn-policy-routing: revert to old gw detection, strip ASCII escape in WebUI

--- luci-app-vpn-policy-routing/luasrc/model/cbi/vpn-policy-routing.lua
+++ luci-app-vpn-policy-routing/luasrc/model/cbi/vpn-policy-routing.lua
@@ -85,6 +85,7 @@ local tmpfsVersion, tmpfsStatus = "", "Stopped"
 if tmpfs and tmpfs['data'] then
 	if tmpfs['data']['status'] and tmpfs['data']['status'] ~= "" then
 		tmpfsStatus = tmpfs['data']['status']
+		tmpfsStatus = tmpfsStatus:gsub('\\033[^ ]*', '✓')
 	end
 	if tmpfs['data']['version'] and tmpfs['data']['version'] ~= "" then
 		tmpfsVersion = " (" .. packageName .. " " .. tmpfs['data']['version'] .. ")"
