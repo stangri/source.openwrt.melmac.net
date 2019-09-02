4353bf51b0fa071a43db0abe7a0fe513cfc9f281
Stan Grishin <stangri@melmac.net>
2019-09-02 05:48:34 -0700

    vpn-policy-routing: create entry in the VPN menu if it exists

--- luci-app-vpn-policy-routing/luasrc/controller/vpn-policy-routing.lua
+++ luci-app-vpn-policy-routing/luasrc/controller/vpn-policy-routing.lua
@@ -1,6 +1,10 @@
 module("luci.controller.vpn-policy-routing", package.seeall)
 function index()
 	if nixio.fs.access("/etc/config/vpn-policy-routing") then
-		entry({"admin", "services", "vpn-policy-routing"}, cbi("vpn-policy-routing"), _("VPN Policy Routing"))
+		if lookup("admin/vpn") then
+			entry({"admin", "vpn", "vpn-policy-routing"}, cbi("vpn-policy-routing"), _("VPN Policy Routing"))
+		else
+			entry({"admin", "services", "vpn-policy-routing"}, cbi("vpn-policy-routing"), _("VPN Policy Routing"))
+		end
 	end
 end
