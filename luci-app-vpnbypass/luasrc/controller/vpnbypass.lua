5a58679f29abba341c125c14d627275e49c964a6
Stan Grishin <stangri@melmac.net>
2019-09-07 10:39:04 -0700

    vpnbypass: better start/stop controls, create entry in the VPN menu if it exists

--- luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
+++ luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
@@ -1,7 +1,7 @@
 module("luci.controller.vpnbypass", package.seeall)
 function index()
 	if nixio.fs.access("/etc/config/vpnbypass") then
-		if lookup("admin/vpn") then
+		if luci.dispatcher.lookup("admin/vpn") then
 			entry({"admin", "vpn", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
 		else
 			entry({"admin", "services", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
