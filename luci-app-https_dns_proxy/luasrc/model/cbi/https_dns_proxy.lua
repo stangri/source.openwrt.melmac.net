bd1fc4e54f416f54374a03bf9e43f5ea647d3911
Stan Grishin <stangri@melmac.net>
2019-10-03 04:22:42 -0700

    https_dns_proxy: Quad9 support, better override of DHCP settings

--- luci-app-https_dns_proxy/luasrc/model/cbi/https_dns_proxy.lua
+++ luci-app-https_dns_proxy/luasrc/model/cbi/https_dns_proxy.lua
@@ -1,4 +1,6 @@
 local uci = require("luci.model.uci").cursor()
+local dispatcher = require("luci.dispatcher")
+
 function uci_del_list(conf, sect, opt, value)
   local lval = uci:get(conf, sect, opt)
   if lval == nil or lval == "" then
@@ -40,29 +42,13 @@ end
 m = Map("https_dns_proxy", translate("HTTPS DNS Proxy Settings"))
 m.template="cbi/map"
 
-s3 = m:section(TypedSection, "https_dns_proxy", translate("Instances"))
+s3 = m:section(TypedSection, "https_dns_proxy", translate("Instances"), translate("When you add/remove any instances below, they will be used to override the 'DNS forwardings' section of ")
+		.. [[ <a href="]] .. dispatcher.build_url("admin/network/dhcp") .. [[">]]
+		.. translate("DHCP and DNS") .. [[</a>]] .. ".")
 s3.template = "cbi/tblsection"
 s3.sortable  = false
 s3.anonymous = true
 s3.addremove = true
-
-local n = 0
-uci:foreach("https_dns_proxy", "https_dns_proxy", function(s)
-    if s[".name"] == section then
-        return false
-    end
-    n = n + 1
-end)
-
-s3.remove = function(self, section)
-  local la_val = uci:get("https_dns_proxy", section, "subnet_addr")
-  local lp_val = uci:get("https_dns_proxy", section, "listen_port")
-  if not la_val or la_val == "" then la_val = "127.0.0.1" end
-  if not lp_val or lp_val == "" then lp_val = n + 5053 end
-  uci_del_list("dhcp", "@dnsmasq[0]", "server", tostring(la_val) .. "#" .. tostring(lp_val))
-  uci:save("dhcp")
-  return TypedSection.remove(self, section)
-end
 
 prov = s3:option(ListValue, "url_prefix", translate("Provider"))
 prov:value("https://cloudflare-dns.com/dns-query?ct=application/dns-json&","Cloudflare")
@@ -71,8 +57,16 @@ prov:value("https://dns.quad9.net:5053/dns-query?","Quad9 (Recommended)")
 prov:value("https://dns9.quad9.net:5053/dns-query?","Quad9 (Secured)")
 prov:value("https://dns10.quad9.net:5053/dns-query?","Quad9 (Unsecured)")
 prov:value("https://dns11.quad9.net:5053/dns-query?","Quad9 (Secured with ECS Support)")
+prov.forcewrite = true
 prov.write = function(self, section, value)
   if not value then return end
+  local n = 0
+  uci:foreach("https_dns_proxy", "https_dns_proxy", function(s)
+      if s[".name"] == section then
+          return false
+      end
+      n = n + 1
+  end)
   local la_val = la:formvalue(section)
   local lp_val = lp:formvalue(section)
   if not la_val or la_val == "" then la_val = "127.0.0.1" end
@@ -110,9 +104,17 @@ la.datatype    = "host"
 la.placeholder = "127.0.0.1"
 la.rmempty     = true
 
+local n = 0
+uci:foreach("https_dns_proxy", "https_dns_proxy", function(s)
+    if s[".name"] == section then
+        return false
+    end
+    n = n + 1
+end)
+
 lp = s3:option(Value, "listen_port", translate("Listen port"))
-lp.datatype    = "port"
-lp.value       = n + 5053
+lp.datatype = "port"
+lp.value    = n + 5053
 
 sa = s3:option(Value, "subnet_addr", translate("Subnet address"))
 sa.datatype = "ip4prefix"
