5ada50af33947ef6a3b86b8dcad1519cd161c1f6
Stan Grishin <stangri@melmac.net>
2019-10-10 05:09:59 -0700

    simple-adblock: formatting fix

--- luci-app-simple-adblock/luasrc/controller/simple-adblock.lua
+++ luci-app-simple-adblock/luasrc/controller/simple-adblock.lua
@@ -1,23 +1,6 @@
 module("luci.controller.simple-adblock", package.seeall)
 function index()
-	if not nixio.fs.access("/etc/config/simple-adblock") then
-		return
-	end
-	entry({"admin", "services", "simple-adblock"}, cbi("simple-adblock"), _("Simple AdBlock"))
-	entry({"admin", "services", "simple-adblock-action"}, post("action_simple_adblock")).leaf = true
-end
-
-function action_simple_adblock()
-	local packageName = "simple-adblock"
-	if luci.http.formvalue("start") then
-		luci.sys.init.start(packageName)
-	elseif luci.http.formvalue("stop") then
-		luci.sys.init.stop(packageName)
-	elseif luci.http.formvalue("enable") then
-		luci.util.exec("uci set " .. packageName .. ".config.enabled=1; uci commit " .. packageName)
-	elseif luci.http.formvalue("disable") then
-		luci.util.exec("uci set " .. packageName .. ".config.enabled=0; uci commit " .. packageName)
-	elseif luci.http.formvalue("dl") then
-		luci.util.exec("/etc/init.d/" .. packageName .. " dl")
+	if nixio.fs.access("/etc/config/simple-adblock") then
+		entry({"admin", "services", "simple-adblock"}, cbi("simple-adblock"), _("Simple AdBlock"))
 	end
 end
