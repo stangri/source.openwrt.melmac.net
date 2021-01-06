d9ad8269c0365f81acb240f23ff842a639b393d5
Stan Grishin <stangri@melmac.net>
2021-01-06 01:59:20 +0000

    https-dns-proxy: add dnscrypt.ca

---
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/ca.dnscrypt.dns1.lua
@@ -0,0 +1,8 @@
+return{
+	name="DnsCryptCa-DNS1",
+	label=_("DNSCrypt.ca (DNS1)"),
+	resolver_url="https://dns1.dnscrypt.ca:453/dns-query",
+	bootstrap_dns="167.114.220.125,149.56.228.45,2607:5300:61:95f:7283:11d9:0f86:e689,2607:5300:120:b9b::200",
+	help_link="https://dnscrypt.ca/",
+	help_link_text="dnscrypt.ca"
+}
