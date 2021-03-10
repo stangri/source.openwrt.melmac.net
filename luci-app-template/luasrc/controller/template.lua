c5ee0b2d4f4cb9ee65914672e91da4c9a7ca1119
Stan Grishin <stangri@melmac.net>
2021-03-10 13:12:49 +0000

    tree-wide: delete obsolete packages

--- luci-app-template/luasrc/controller/template.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.template", package.seeall)
-function index()
-	if nixio.fs.access("/etc/config/template") then
-		entry({"admin", "services", "template"}, cbi("template"), _("Template")).acl_depends = { "luci-app-template" }
-	end
-end
