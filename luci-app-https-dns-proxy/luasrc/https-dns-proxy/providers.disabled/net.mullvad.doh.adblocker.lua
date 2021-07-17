83919f9eb51b67a39f0f807143df7fc301822cae
Stan Grishin <stangri@melmac.net>
2021-07-17 18:51:14 +0000

    luci-app-https-dns-proxy: add secondary bootstrap to entries with a single bootstrap server

---
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers.disabled/net.mullvad.doh.adblocker.lua
@@ -0,0 +1,8 @@
+return {
+	name="mullvad-adblock",
+	label=_("Mullvad (AdBlock)"),
+	resolver_url="https://adblock.doh.mullvad.net/dns-query",
+	bootstrap_dns="193.138.218.74,1.1.1.1",
+	help_link="https://mullvad.net/en/help/dns-over-https-and-dns-over-tls/",
+	help_link_text="Mullvad.net"
+}
