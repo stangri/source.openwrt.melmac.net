55a20721ce6808e07b928744b31f61c72c696647
Stan Grishin <stangri@melmac.net>
2021-07-29 05:18:55 +0000

    https-dns-proxy: sanitize bootstrap dns

--- luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
@@ -2,6 +2,6 @@ return {
 	name = "Google",
 	label = _("Google"),
 	resolver_url = "https://dns.google/dns-query",
-	bootstrap_dns = "8.8.8.8,8.8.4.4",
+	bootstrap_dns = "8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844",
 	default = true
 }
