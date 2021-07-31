e304fb1c5ee2bced016b0cb2c8bb3c0acd1f97c1
Stan Grishin <stangri@melmac.net>
2021-07-31 10:14:38 +0000

    https-dns-proxy: support for http2_only resolvers

--- luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers.disabled/net.mullvad.doh.lua
+++
@@ -1,8 +0,0 @@
-return {
-	name="mullvad",
-	label=_("Mullvad"),
-	resolver_url="https://doh.mullvad.net/dns-query",
-	bootstrap_dns="193.138.218.74,1.1.1.1",
-	help_link="https://mullvad.net/en/help/dns-over-https-and-dns-over-tls/",
-	help_link_text="Mullvad.net"
-}
