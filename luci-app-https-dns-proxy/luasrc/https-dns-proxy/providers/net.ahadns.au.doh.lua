64f21aa95a3f0a5d75ceeb0a6b94b012762a4bd6
Stan Grishin <stangri@melmac.ca>
2021-12-24 00:40:15 +0000

    https-dns-proxy: fix bootstrap_dns for AhaDNS

--- luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/net.ahadns.au.doh.lua
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/net.ahadns.au.doh.lua
@@ -2,7 +2,7 @@ return {
 	name = "ahadns-doh-au",
 	label = _("AhaDNS.com - Australia (Block Malware + Ads)"),
 	resolver_url = "https://doh.au.ahadns.net/dns-query",
-	bootstrap_dns = "103.73.64.132,2406:ef80:100:11::11",
+	bootstrap_dns = "185.213.26.187,45.67.219.208,5.2.75.75,45.79.120.233,2a0d:5600:33:3::3,2a04:bdc7:100:70::70,2a04:52c0:101:75::75,2400:8904:e001:43::43",
 	help_link = "https://ahadns.com/dns-over-https/",
 	help_link_text = "AhaDNS.com"
 }
