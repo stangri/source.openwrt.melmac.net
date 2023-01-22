8d392e47badd09bcb45c2d5520509f3c22fa7449
Aleem <5023467+ByteAndNibble@users.noreply.github.com>
2023-01-22 17:18:31 +0000

    use dummy string when custom profiles aren't offered

--- luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
+++ luci-app-https-dns-proxy/luasrc/https-dns-proxy/providers/google.dns.lua
@@ -2,5 +2,6 @@ return {
 	name = "dns.google",
 	label = _("Google"),
 	resolver_url = "https://dns.google/dns-query",
+	provider_domain = "https.dns.proxy"
 	bootstrap_dns = "8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844"
 }
