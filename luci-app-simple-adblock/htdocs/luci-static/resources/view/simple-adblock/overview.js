ebce504a5f75cda447b4c1353bc0d6e09aee3514
Stan Grishin <stangri@melmac.ca>
2023-07-28 23:21:23 +0000

    simple-adblock: webui fix
    Signed-off-by: Stan Grishin <stangri@melmac.ca>

--- luci-app-simple-adblock/htdocs/luci-static/resources/view/simple-adblock/overview.js
+++ luci-app-simple-adblock/htdocs/luci-static/resources/view/simple-adblock/overview.js
@@ -71,10 +71,10 @@ return view.extend({
 			}
 			else {
 				if (!(replyPlatform[pkg.Name].dnsmasq_ipset_support)) {
-					text += _("Please note that %s is not supported on this system.").format("<i>dnsmasq.ipset</i>") + "<br />";
+					text += "<br />" + _("Please note that %s is not supported on this system.").format("<i>dnsmasq.ipset</i>");
 				}
 				if (!(replyPlatform[pkg.Name].dnsmasq_nftset_support)) {
-					text += _("Please note that %s is not supported on this system.").format("<i>dnsmasq.nftset</i>") + "<br />";
+					text += "<br />" + _("Please note that %s is not supported on this system.").format("<i>dnsmasq.nftset</i>");
 				}
 			}
 			if (!(replyPlatform[pkg.Name].unbound_installed)) {
