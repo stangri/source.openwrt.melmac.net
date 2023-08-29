1e2cad761202ffb402628167c9afdb7f24fa8cc4
Stan Grishin <stangri@melmac.ca>
2023-08-29 20:38:29 +0000

    adblock-fast: better humanFileSize

--- luci-app-adblock-fast/htdocs/luci-static/resources/view/adblock-fast/overview.js
+++ luci-app-adblock-fast/htdocs/luci-static/resources/view/adblock-fast/overview.js
@@ -16,29 +16,8 @@ var pkg = {
 	get URL() {
 		return "https://docs.openwrt.melmac.net/" + pkg.Name + "/";
 	},
-	// https://stackoverflow.com/a/14919494
-	humanFileSize: function (bytes, si = false, dp = 1) {
-		const thresh = si ? 1000 : 1024;
-
-		if (Math.abs(bytes) < thresh) {
-			return bytes + " B";
-		}
-
-		const units = si
-			? ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
-			: ["KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
-		let u = -1;
-		const r = 10 ** dp;
-
-		do {
-			bytes /= thresh;
-			++u;
-		} while (
-			Math.round(Math.abs(bytes) * r) / r >= thresh &&
-			u < units.length - 1
-		);
-
-		return bytes.toFixed(dp) + " " + units[u];
+	humanFileSize: function (bytes, si = false, dp = 2) {
+		return `%${si ? 1000 : 1024}.${dp ?? 0}mB`.format(bytes);
 	},
 };
 
