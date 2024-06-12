1a387b68ea681043159095c20560d86729673ab7
Stan Grishin <stangri@melmac.ca>
2024-06-12 20:50:52 +0000

    Removing the luci-app-fakeinternet folder, that is now a repository
    Signed-off-by: Stan Grishin <stangri@melmac.ca>

--- luci-app-fakeinternet/luasrc/controller/fakeinternet.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.fakeinternet", package.seeall)
-function index()
-	if nixio.fs.access("/etc/config/fakeinternet") then
-		entry({"admin", "services", "fakeinternet"}, cbi("fakeinternet"), _("Fakeinternet")).acl_depends = { "luci-app-fakeinternet" }
-	end
-end
